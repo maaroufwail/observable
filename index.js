d46cdf6f0fd73960b1a962ee5509a795120d3616
maaroufwail <51120571+maaroufwail@users.noreply.github.com>
2019-06-07 16:02:30 +0200

    combinelatest

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-const { fromEvent, of, interval } = rxjs;
+const { fromEvent, of, combineLatest } = rxjs;
 const { map, tap, switchAll } = rxjs.operators;
 
 const input = document.querySelector("input");
 const select = document.querySelector("select");
 const output = document.querySelector("output");
 
-const id$ = fromEvent(input, "input").map(e => e.target.value);
-
-const resource$ = fromEvent(select, "change").map(e => e.target.value);
-combineLatest(id$, resource$);
+/*const control = fromEvent(document, "click").subscribe(e =>
+  console.log("clicked")
+);*/
+const rating$ = fromEvent(input, "input").pipe(map(e => e.target.value));
+const type$ = fromEvent(select, "change").pipe(map(e => e.target.value));
+combineLatest(rating$, type$).subscribe(e =>
+  console.log(rating$ + "   " + id$)
+);
